50da3c9664407b7d2ad42fe05b24042f133fde00
Wibulization <nhmkhoi123@gmail.com>
2024-07-18 14:51:16 +0700

    Error 2

--- engine/src/main/java/io/beandev/datorum/migration/jdbc/JdbcMigrationRepository.java
+++ engine/src/main/java/io/beandev/datorum/migration/jdbc/JdbcMigrationRepository.java
@@ -50,7 +50,7 @@ public class JdbcMigrationRepository implements MigrationRepository {
             }
 
             // save differences in difference table
-            String insertDifferenceSql = "INSERT INTO datorum_schema.difference (id, migration_parent_id, migration_id, name, scope, action, data_type, relation_entity_id, owner_entity_id) VALUES (?, ?, ?, ?, ?::datorum_schema.scopeenum, ?::datorum_schema.differenceactionenum, ?, ?, ?)";
+            String insertDifferenceSql = "INSERT INTO datorum_schema.difference (id, migration_parent_id, migration_id, name, scope, action, data_type, relation_entity_id, owner_entity_id) VALUES (?, ?, ?, ?, ?::datorum_schema.scopeenum, ?::datorum_schema.differenceactionenum, ?::datorum_schema.datatype, ?, ?)";
             try (PreparedStatement stmt = conn.prepareStatement(insertDifferenceSql)) {
                 for (Difference difference : migration.differences()) {
                     stmt.setLong(1, difference.id());
@@ -91,87 +91,88 @@ public class JdbcMigrationRepository implements MigrationRepository {
             try (Statement stmt = conn.createStatement()) {
                 // Check the types
                 ResultSet rs = stmt.executeQuery("""
-                    SELECT *
-                    FROM pg_catalog.pg_type
-                             JOIN pg_catalog.pg_namespace
-                                  ON pg_type.typnamespace = pg_namespace.oid
-                    WHERE typname = '_scopeenum' 
-                      AND pg_namespace.nspname = 'datorum_schema' 
-                      AND pg_type.typtype = 'b' 
-            """);
+                                SELECT *
+                                FROM pg_catalog.pg_type
+                                         JOIN pg_catalog.pg_namespace
+                                              ON pg_type.typnamespace = pg_namespace.oid
+                                WHERE typname = '_scopeenum'
+                                  AND pg_namespace.nspname = 'datorum_schema'
+                                  AND pg_type.typtype = 'b'
+                        """);
 
                 if (rs.next()) {
                     return;
                 }
                 // Execute TYPE creation
                 stmt.execute("""
-                        CREATE TYPE datorum_schema.ScopeEnum AS ENUM(
-                            'APP',
-                            'CONTEXT',
-                            'AGGREGATE',
-                            'ENTITY',
-                            'ATTRIBUTE'
-                        );
+                            CREATE TYPE datorum_schema.ScopeEnum AS ENUM(
+                                'APP',
+                                'CONTEXT',
+                                'AGGREGATE',
+                                'ENTITY',
+                                'ATTRIBUTE'
+                            );
 
-                        CREATE TYPE datorum_schema.DifferenceActionEnum AS ENUM (
-                            'CREATE',
-                            'RENAME',
-                            'CHANGE_ATTRIBUTE_OWNER',
-                            'CHANGE_DATA_TYPE',
-                            'INCREASE',
-                            'DECREASE',
-                            'DELETE'
-                        );
+                            CREATE TYPE datorum_schema.DifferenceActionEnum AS ENUM (
+                                'CREATE',
+                                'RENAME',
+                                'CHANGE_ATTRIBUTE_OWNER',
+                                'CHANGE_DATA_TYPE',
+                                'INCREASE',
+                                'DECREASE',
+                                'DELETE'
+                            );
 
-                        CREATE TYPE datorum_schema.DifferenceType AS (
-                            id        BIGINT,
-                            name      VARCHAR(255),
-                            scope     datorum_schema.ScopeEnum,
-                            action    datorum_schema.DifferenceActionEnum,
-                            data_type datorum_schema.DataType,
-                            relation  datorum_schema.EntityType,
-                            owner     datorum_schema.EntityType
-                        );
+                            CREATE TYPE datorum_schema.DifferenceType AS (
+                                id        BIGINT,
+                                name      VARCHAR(255),
+                                scope     datorum_schema.ScopeEnum,
+                                action    datorum_schema.DifferenceActionEnum,
+                                data_type datorum_schema.DataType,
+                                relation  datorum_schema.EntityType,
+                                owner     datorum_schema.EntityType
+                            );
 
-                        CREATE TYPE datorum_schema.StatusEnum AS ENUM (
-                            'IN_PROGRESS',
-                            'COMPLETED',
-                            'FAILED',
-                            'PENDING'
-                        );
-                    """);
+                            CREATE TYPE datorum_schema.StatusEnum AS ENUM (
+                                'IN_PROGRESS',
+                                'COMPLETED',
+                                'FAILED',
+                                'PENDING'
+                            );
+                        """);
 
-                        // Execute table creation and data insertion
-                        stmt.execute("""
-                        CREATE TABLE IF NOT EXISTS datorum_schema.migration (
-                            parent_id              BIGINT,
-                            id                     BIGINT,
-                            previous_parent_id     BIGINT, 
-                            previous_id            BIGINT,
-                            hash                   VARCHAR(40),
-                            status                 datorum_schema.StatusEnum,
-                            PRIMARY KEY (parent_id, id),
-                            FOREIGN KEY (previous_parent_id, previous_id) REFERENCES datorum_schema.migration(parent_id, id)                                
-                        );
+                // Execute table creation and data insertion
+                stmt.execute(
+                        """
+                                    CREATE TABLE IF NOT EXISTS datorum_schema.migration (
+                                        parent_id              BIGINT,
+                                        id                     BIGINT,
+                                        previous_parent_id     BIGINT,
+                                        previous_id            BIGINT,
+                                        hash                   VARCHAR(40),
+                                        status                 datorum_schema.StatusEnum,
+                                        PRIMARY KEY (parent_id, id),
+                                        FOREIGN KEY (previous_parent_id, previous_id) REFERENCES datorum_schema.migration(parent_id, id)
+                                    );
 
-                        CREATE TABLE IF NOT EXISTS datorum_schema.difference (
-                            id                     BIGINT PRIMARY KEY,
-                            migration_parent_id    BIGINT,
-                            migration_id           BIGINT,
-                            name                VARCHAR(255),
-                            scope               datorum_schema.ScopeEnum,
-                            action              datorum_schema.DifferenceActionEnum,
-                            data_type           datorum_schema.DataType,
-                            relation_entity_id  BIGINT,
-                            owner_entity_id     BIGINT,                            
-                            FOREIGN KEY (migration_parent_id, migration_id) REFERENCES datorum_schema.migration(parent_id, id)                                
-                        );
+                                    CREATE TABLE IF NOT EXISTS datorum_schema.difference (
+                                        id                     BIGINT PRIMARY KEY,
+                                        migration_parent_id    BIGINT,
+                                        migration_id           BIGINT,
+                                        name                VARCHAR(255),
+                                        scope               datorum_schema.ScopeEnum,
+                                        action              datorum_schema.DifferenceActionEnum,
+                                        data_type           datorum_schema.DataType,
+                                        relation_entity_id  BIGINT,
+                                        owner_entity_id     BIGINT,
+                                        FOREIGN KEY (migration_parent_id, migration_id) REFERENCES datorum_schema.migration(parent_id, id)
+                                    );
 
-                        CREATE INDEX difference__migration_id ON datorum_schema.difference (migration_parent_id, migration_id);
+                                    CREATE INDEX difference__migration_id ON datorum_schema.difference (migration_parent_id, migration_id);
 
-                        INSERT INTO datorum_schema.system_info (property_name, property_value) 
-                        VALUES ('migration.version', 'v1.0.0');
-                    """);
+                                    INSERT INTO datorum_schema.system_info (property_name, property_value)
+                                    VALUES ('migration.version', 'v1.0.0');
+                                """);
                 conn.commit();
             } catch (Exception e) {
                 conn.rollback();
